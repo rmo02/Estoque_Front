a6aa3ea5b86a787d2d38bec68be28b9fcf746e68
Artho Eduardo Rodrigues Pereira <artho.pereira@mirante.com.br>
2024-09-17 12:43:36 -0300

    categoria adicionada e status atualizado

--- src/components/Modals/AddItem/index.jsx
+++ src/components/Modals/AddItem/index.jsx
@@ -5,6 +5,8 @@ import Swal from 'sweetalert2'
 
 export function AddItem({ closeModal }) {
   const [name, setName] = useState('')
+  const [categoria, setCategoria] = useState('')
+  const [category, setCategory] = useState('')
   const [naPrateleira, setNaPrateleira] = useState('true')
   const [prateleira, setPrateleira] = useState([])
   const [shelf, setShelf] = useState('')
@@ -13,15 +15,9 @@ export function AddItem({ closeModal }) {
   const [linha, setLinha] = useState('')
   const [coluna, setColuna] = useState('')
   const [file, setFile] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [status, setStatus] = useState('') // Define o estado para o status
-  const [cards, setCards] = useState([])
+  const [status, setStatus] = useState('')
 
   useEffect(() => {
-    const getEquipments = async () => {
-      const response = await app.get('/equipment')
-      setCards(response.data)
-    }
     const getShelves = async () => {
       const response = await app.get('/shelves')
       setPrateleira(response.data)
@@ -30,7 +26,6 @@ export function AddItem({ closeModal }) {
       const response = await app.get('/section')
       setSecao(response.data)
     }
-    getEquipments()
     getShelves()
     getSections()
   }, [])
@@ -48,6 +43,7 @@ export function AddItem({ closeModal }) {
 
     const formData = new FormData()
     formData.append('name', name)
+    formData.append('categoryId', category)
     formData.append('inShelf', naPrateleira)
     formData.append('linha', linha)
     formData.append('column', coluna)
@@ -100,8 +96,17 @@ export function AddItem({ closeModal }) {
     }
   })
 
+  const handleCheckboxChange = e => {
+    const { value, checked } = e.target
+    if (checked) {
+      setStatus(value) // Atualiza o estado com o valor do checkbox selecionado
+    } else {
+      setStatus('') // Desmarca todos (opcional)
+    }
+  }
+
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col h-full w-full justify-between">
       <div className="flex flex-row justify-between items-center font-semibold">
         <p>Adicionar item</p>
         <MdClose
@@ -110,143 +115,186 @@ export function AddItem({ closeModal }) {
         />
       </div>
       <p>Preencha os campos abaixo para adicionar um novo item ao estoque</p>
-      <form>
-        <div>
-          <div className="flex flex-row justify-between items-center space-x-4 py-2 w-full">
-            <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
-              <label htmlFor="Name">Nome</label>
-              <input
-                type="name"
-                className="border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue w-full text-sm"
-                id="Name"
-                placeholder="Digite o nome do item"
-                onChange={e => setName(e.target.value)}
-              />
-            </div>
-            <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
-              <label htmlFor="inputState">Tipo de Armazenamento</label>
-              <select
-                id="inputState"
-                onChange={handleStorageTypeChange}
-                className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
-                value={naPrateleira} // Garante que o valor correto está refletido
-              >
-                <option value="" disabled>
-                  Selecione o Tipo de Armazenamento
-                </option>
-                <option value="true">Prateleira</option>
-                <option value="false">Seção</option>
-              </select>
-            </div>
+      <form className="flex flex-col space-y-2">
+        <div className="flex flex-row justify-between items-center space-x-4 w-full">
+          {/* NOME */}
+          <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
+            <label htmlFor="Name">Nome</label>
+            <input
+              type="name"
+              className="border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue w-full text-sm"
+              id="Name"
+              placeholder="Digite o nome do item"
+              onChange={e => setName(e.target.value)}
+            />
           </div>
-          <div className="flex flex-row justify-between items-center space-x-4 py-2 w-full">
-            {naPrateleira === 'true' ? (
-              <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
-                <label htmlFor="inputState">Prateleira</label>
-                <select
-                  onChange={e => setShelf(e.target.value)}
-                  id="inputState"
-                  className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
-                  value={shelf === null ? '' : shelf} // Garante que o valor de shelf é refletido corretamente
-                >
-                  <option value="" disabled>
-                    Selecione a Prateleira
-                  </option>
-                  {prateleira
-                    .sort((a, b) => a.name.localeCompare(b.name))
-                    .map((item, index) => {
-                      return (
-                        <option key={index + 1} value={item.id}>
-                          {item.name}
-                        </option>
-                      )
-                    })}
-                </select>
-              </div>
-            ) : (
-              <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
-                <label htmlFor="inputState">Seção</label>
-                <select
-                  onChange={e => setSection(e.target.value)}
-                  id="inputState"
-                  className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
-                  value={section === null ? '' : section} // Garante que o valor de section é refletido corretamente
-                >
-                  <option value="" disabled>
-                    Selecione a Seção
-                  </option>
-                  {secao
-                    .sort((a, b) => a.name.localeCompare(b.name))
-                    .map((item, index) => {
-                      return (
-                        <option key={index + 1} value={item.id}>
-                          {item.name}
-                        </option>
-                      )
-                    })}
-                </select>
-              </div>
-            )}
-
-            {/* STATUS */}
-            <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
-              <label htmlFor="inputState">Status</label>
-              <select
-                id="inputState"
-                onChange={e => setStatus(e.target.value)}
-                className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
-                value={status}
-              >
-                <option value="" disabled>
-                  Selecione o Status
+          <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
+            <label htmlFor="inputState">Categoria</label>
+            <select
+              id="inputState"
+              onChange={e => setCategoria(e.target.value)}
+              className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
+              value={categoria}
+            >
+              <option value="" disabled>
+                Selecione a Categoria
+              </option>
+              {[...Array(8)].map((item, index) => (
+                <option key={index + 1} value={index + 1}>
+                  Categoria {index + 1}
                 </option>
-                <option value="DISPONIVEL">Disponível</option>
-                <option value="EM_USO">Em Uso</option>
-                <option value="EM_MANUTENCAO">Em Manutenção</option>
-              </select>
-            </div>
-          </div>
-
-          {/* COLUNA E LINHA */}
-          <div className="flex flex-row justify-between items-center space-x-4 py-2 w-full">
-            <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
-              <label htmlFor="inputState">Coluna</label>
-              <select
-                id="inputState"
-                onChange={e => setColuna(e.target.value)}
-                className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
-                value={coluna}
-              >
-                <option value="" disabled>
-                  Selecione a Coluna
-                </option>
-                {[...Array(8)].map((item, index) => (
-                  <option key={index + 1} value={index + 1}>
-                    Coluna {index + 1}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
-              <label htmlFor="inputState">Linha</label>
-              <select
-                id="inputState"
-                onChange={e => setLinha(e.target.value)}
-                className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
-                value={linha}
-              >
-                <option value="" disabled>
-                  Selecione a Linha
-                </option>
-                {[...Array(8)].map((item, index) => (
-                  <option key={index + 1} value={index + 1}>
-                    Linha {index + 1}
-                  </option>
-                ))}
-              </select>
-            </div>
+              ))}
+            </select>
           </div>
         </div>
+
+        <div className="flex flex-row justify-between items-center space-x-4 w-full">
+          {/* TIPO DE ARMAZENAMENTO */}
+          <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
+            <label htmlFor="inputState">Tipo de Armazenamento</label>
+            <select
+              id="inputState"
+              onChange={handleStorageTypeChange}
+              className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
+              value={naPrateleira} // Garante que o valor correto está refletido
+            >
+              <option value="" disabled>
+                Selecione o Tipo de Armazenamento
+              </option>
+              <option value="true">Prateleira</option>
+              <option value="false">Seção</option>
+            </select>
+          </div>
+          {naPrateleira === 'true' ? (
+            // PRATELEIRA
+            <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
+              <label htmlFor="inputState">Prateleira</label>
+              <select
+                onChange={e => setShelf(e.target.value)}
+                id="inputState"
+                className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
+                value={shelf === null ? '' : shelf} // Garante que o valor de shelf é refletido corretamente
+              >
+                <option value="" disabled>
+                  Selecione a Prateleira
+                </option>
+                {prateleira
+                  .sort((a, b) => a.name.localeCompare(b.name))
+                  .map((item, index) => {
+                    return (
+                      <option key={index + 1} value={item.id}>
+                        {item.name}
+                      </option>
+                    )
+                  })}
+              </select>
+            </div>
+          ) : (
+            // SEÇÃO
+            <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
+              <label htmlFor="inputState">Seção</label>
+              <select
+                onChange={e => setSection(e.target.value)}
+                id="inputState"
+                className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
+                value={section === null ? '' : section} // Garante que o valor de section é refletido corretamente
+              >
+                <option value="" disabled>
+                  Selecione a Seção
+                </option>
+                {secao
+                  .sort((a, b) => a.name.localeCompare(b.name))
+                  .map((item, index) => {
+                    return (
+                      <option key={index + 1} value={item.id}>
+                        {item.name}
+                      </option>
+                    )
+                  })}
+              </select>
+            </div>
+          )}
+        </div>
+
+        {/* COLUNA E LINHA */}
+        <div className="flex flex-row justify-between items-center space-x-4 w-full">
+          <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
+            <label htmlFor="inputState">Coluna</label>
+            <select
+              id="inputState"
+              onChange={e => setColuna(e.target.value)}
+              className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
+              value={coluna}
+            >
+              <option value="" disabled>
+                Selecione a Coluna
+              </option>
+              {[...Array(8)].map((item, index) => (
+                <option key={index + 1} value={index + 1}>
+                  Coluna {index + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col justify-start items-start w-1/2 space-y-1">
+            <label htmlFor="inputState">Linha</label>
+            <select
+              id="inputState"
+              onChange={e => setLinha(e.target.value)}
+              className="w-full border border-0.5 border-color_grey rounded-md p-1 focus:outline-color_blue text-sm"
+              value={linha}
+            >
+              <option value="" disabled>
+                Selecione a Linha
+              </option>
+              {[...Array(8)].map((item, index) => (
+                <option key={index + 1} value={index + 1}>
+                  Linha {index + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        {/* STATUS */}
+        <div className="flex flex-col justify-start items-start w-full space-y-1">
+          <label>Status</label>
+          <div className="flex flex-row w-full space-x-4 text-base ">
+            <label className="flex  items-center">
+              <input
+                className="w-4 h-4 rounded-full appearance-none border border-gray-400 checked:bg-blue-600 checked:border-transparent focus:outline-none"
+                type="checkbox"
+                value="DISPONIVEL"
+                checked={status === 'DISPONIVEL'}
+                onChange={e => handleCheckboxChange(e)}
+              />
+              <span className="ml-2">Disponível</span>
+            </label>
+
+            <label>
+              <input
+                className="w-4 h-4 rounded-full appearance-none border border-gray-400 checked:bg-blue-600 checked:border-transparent focus:outline-none"
+                type="checkbox"
+                value="EM_USO"
+                checked={status === 'EM_USO'}
+                onChange={e => handleCheckboxChange(e)}
+              />
+              <span className="ml-2">Em Uso</span>
+            </label>
+            <label>
+              <input
+                className="w-4 h-4 rounded-full appearance-none border border-gray-400 checked:bg-blue-600 checked:border-transparent focus:outline-none"
+                type="checkbox"
+                value="EM_MANUTENCAO"
+                checked={status === 'EM_MANUTENCAO'}
+                onChange={e => handleCheckboxChange(e)}
+              />
+              <span className="ml-2">Em Manutenção</span>
+            </label>
+          </div>
+        </div>
+
+        {/* ADICIONAR IMAGEM */}
         <div className="flex flex-col justify-start w-full space-y-1">
           <label htmlFor="Image">Imagem</label>
           <input
@@ -256,7 +304,9 @@ export function AddItem({ closeModal }) {
             onChange={e => setFile(e.target.files[0])}
           />
         </div>
-        <div className="flex flex-row justify-end space-x-2 py-5">
+
+        {/* CANCELAR E SALVAR */}
+        <div className="flex flex-row justify-end space-x-2">
           <button
             className="text-white bg-color_grey hover:opacity-80 rounded-md py-1 px-2"
             onClick={closeModal}
